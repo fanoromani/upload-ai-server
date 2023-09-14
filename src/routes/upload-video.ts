62e93937ce38253ebb70849f3a55610ed3a3f958
Stefano Romani <fanoromani@gmail.com>
2023-09-14 11:48:45 -0300

    upload video route

--- src/routes/upload-video.ts
+++ src/routes/upload-video.ts
@@ -1,6 +1,13 @@
 import { FastifyInstance } from "fastify";
 import { fastifyMultipart } from "@fastify/multipart";
 import path from "node:path";
+import { randomUUID } from "node:crypto";
+import fs from "node:fs";
+import { pipeline } from "node:stream";
+import { promisify } from "node:util";
+import { prisma } from "../lib/prisma";
+
+const pump = promisify(pipeline);
 
 export async function uploadVideoRoute(app: FastifyInstance) {
   app.register(fastifyMultipart, { limits: { fileSize: 1048576 * 25 } });
@@ -15,5 +22,22 @@ export async function uploadVideoRoute(app: FastifyInstance) {
         .status(400)
         .send({ error: "Invalid input type, please upload an MP3" });
     }
+    const fileBaseName = path.basename(data.filename, extension);
+    const fileUploadName = `${fileBaseName}-${randomUUID()}${extension}`;
+    const uploadDestination = path.resolve(
+      __dirname,
+      "../../temp",
+      fileUploadName
+    );
+    await pump(data.file, fs.createWriteStream(uploadDestination));
+
+    const video = await prisma.video.create({
+      data: {
+        name: data.filename,
+        path: uploadDestination,
+      },
+    });
+
+    return { video };
   });
 }
